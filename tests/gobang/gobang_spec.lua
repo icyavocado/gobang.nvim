ada23730e2a3cf47aaa1fd04c58fd858e5ce52c9
Dai <dainsait@gmail.com>
2024-04-15 09:15:25 -0600

    [REMOVE] tests

--- tests/gobang/gobang_spec.lua
+++ tests/gobang/gobang_spec.lua
@@ -1,12 +1,1 @@
 local plugin = require("gobang")
-
-describe("setup", function()
-  it("works with default", function()
-    assert(plugin.gobang() == "Hello!", "my first function with param = Hello!")
-  end)
-
-  it("works with custom var", function()
-    plugin.setup({ opt = "custom" })
-    assert(plugin.gobang() == "custom", "my first function with param = custom")
-  end)
-end)
